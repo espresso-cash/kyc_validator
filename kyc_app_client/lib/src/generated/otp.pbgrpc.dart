64d1d65ee11d9315b11e3ad187d31addbc99494a
Justin Enerio <justinenerio@pm.me>
2024-08-04 11:21:36 +0700

    feat: updated dependencies

--- kyc_app_client/lib/src/generated/otp.pbgrpc.dart
+++ kyc_app_client/lib/src/generated/otp.pbgrpc.dart
@@ -15,24 +15,24 @@ import 'dart:core' as $core;
 import 'package:grpc/service_api.dart' as $grpc;
 import 'package:protobuf/protobuf.dart' as $pb;
 
-import 'otp.pb.dart' as $0;
+import 'otp.pb.dart' as $1;
 
 export 'otp.pb.dart';
 
 @$pb.GrpcServiceName('kyc.OtpService')
 class OtpServiceClient extends $grpc.Client {
-  static final _$sendOtpByEmail = $grpc.ClientMethod<$0.SendOtpRequest, $0.SendOtpResponse>(
+  static final _$sendOtpByEmail = $grpc.ClientMethod<$1.SendOtpRequest, $1.SendOtpResponse>(
       '/kyc.OtpService/SendOtpByEmail',
-      ($0.SendOtpRequest value) => value.writeToBuffer(),
-      ($core.List<$core.int> value) => $0.SendOtpResponse.fromBuffer(value));
-  static final _$sendOtpBySms = $grpc.ClientMethod<$0.SendOtpRequest, $0.SendOtpResponse>(
+      ($1.SendOtpRequest value) => value.writeToBuffer(),
+      ($core.List<$core.int> value) => $1.SendOtpResponse.fromBuffer(value));
+  static final _$sendOtpBySms = $grpc.ClientMethod<$1.SendOtpRequest, $1.SendOtpResponse>(
       '/kyc.OtpService/SendOtpBySms',
-      ($0.SendOtpRequest value) => value.writeToBuffer(),
-      ($core.List<$core.int> value) => $0.SendOtpResponse.fromBuffer(value));
-  static final _$verifyOtp = $grpc.ClientMethod<$0.VerifyOtpRequest, $0.VerifyOtpResponse>(
+      ($1.SendOtpRequest value) => value.writeToBuffer(),
+      ($core.List<$core.int> value) => $1.SendOtpResponse.fromBuffer(value));
+  static final _$verifyOtp = $grpc.ClientMethod<$1.VerifyOtpRequest, $1.VerifyOtpResponse>(
       '/kyc.OtpService/VerifyOtp',
-      ($0.VerifyOtpRequest value) => value.writeToBuffer(),
-      ($core.List<$core.int> value) => $0.VerifyOtpResponse.fromBuffer(value));
+      ($1.VerifyOtpRequest value) => value.writeToBuffer(),
+      ($core.List<$core.int> value) => $1.VerifyOtpResponse.fromBuffer(value));
 
   OtpServiceClient($grpc.ClientChannel channel,
       {$grpc.CallOptions? options,
@@ -40,15 +40,15 @@ class OtpServiceClient extends $grpc.Client {
       : super(channel, options: options,
         interceptors: interceptors);
 
-  $grpc.ResponseFuture<$0.SendOtpResponse> sendOtpByEmail($0.SendOtpRequest request, {$grpc.CallOptions? options}) {
+  $grpc.ResponseFuture<$1.SendOtpResponse> sendOtpByEmail($1.SendOtpRequest request, {$grpc.CallOptions? options}) {
     return $createUnaryCall(_$sendOtpByEmail, request, options: options);
   }
 
-  $grpc.ResponseFuture<$0.SendOtpResponse> sendOtpBySms($0.SendOtpRequest request, {$grpc.CallOptions? options}) {
+  $grpc.ResponseFuture<$1.SendOtpResponse> sendOtpBySms($1.SendOtpRequest request, {$grpc.CallOptions? options}) {
     return $createUnaryCall(_$sendOtpBySms, request, options: options);
   }
 
-  $grpc.ResponseFuture<$0.VerifyOtpResponse> verifyOtp($0.VerifyOtpRequest request, {$grpc.CallOptions? options}) {
+  $grpc.ResponseFuture<$1.VerifyOtpResponse> verifyOtp($1.VerifyOtpRequest request, {$grpc.CallOptions? options}) {
     return $createUnaryCall(_$verifyOtp, request, options: options);
   }
 }
@@ -58,42 +58,42 @@ abstract class OtpServiceBase extends $grpc.Service {
   $core.String get $name => 'kyc.OtpService';
 
   OtpServiceBase() {
-    $addMethod($grpc.ServiceMethod<$0.SendOtpRequest, $0.SendOtpResponse>(
+    $addMethod($grpc.ServiceMethod<$1.SendOtpRequest, $1.SendOtpResponse>(
         'SendOtpByEmail',
         sendOtpByEmail_Pre,
         false,
         false,
-        ($core.List<$core.int> value) => $0.SendOtpRequest.fromBuffer(value),
-        ($0.SendOtpResponse value) => value.writeToBuffer()));
-    $addMethod($grpc.ServiceMethod<$0.SendOtpRequest, $0.SendOtpResponse>(
+        ($core.List<$core.int> value) => $1.SendOtpRequest.fromBuffer(value),
+        ($1.SendOtpResponse value) => value.writeToBuffer()));
+    $addMethod($grpc.ServiceMethod<$1.SendOtpRequest, $1.SendOtpResponse>(
         'SendOtpBySms',
         sendOtpBySms_Pre,
         false,
         false,
-        ($core.List<$core.int> value) => $0.SendOtpRequest.fromBuffer(value),
-        ($0.SendOtpResponse value) => value.writeToBuffer()));
-    $addMethod($grpc.ServiceMethod<$0.VerifyOtpRequest, $0.VerifyOtpResponse>(
+        ($core.List<$core.int> value) => $1.SendOtpRequest.fromBuffer(value),
+        ($1.SendOtpResponse value) => value.writeToBuffer()));
+    $addMethod($grpc.ServiceMethod<$1.VerifyOtpRequest, $1.VerifyOtpResponse>(
         'VerifyOtp',
         verifyOtp_Pre,
         false,
         false,
-        ($core.List<$core.int> value) => $0.VerifyOtpRequest.fromBuffer(value),
-        ($0.VerifyOtpResponse value) => value.writeToBuffer()));
+        ($core.List<$core.int> value) => $1.VerifyOtpRequest.fromBuffer(value),
+        ($1.VerifyOtpResponse value) => value.writeToBuffer()));
   }
 
-  $async.Future<$0.SendOtpResponse> sendOtpByEmail_Pre($grpc.ServiceCall call, $async.Future<$0.SendOtpRequest> request) async {
+  $async.Future<$1.SendOtpResponse> sendOtpByEmail_Pre($grpc.ServiceCall call, $async.Future<$1.SendOtpRequest> request) async {
     return sendOtpByEmail(call, await request);
   }
 
-  $async.Future<$0.SendOtpResponse> sendOtpBySms_Pre($grpc.ServiceCall call, $async.Future<$0.SendOtpRequest> request) async {
+  $async.Future<$1.SendOtpResponse> sendOtpBySms_Pre($grpc.ServiceCall call, $async.Future<$1.SendOtpRequest> request) async {
     return sendOtpBySms(call, await request);
   }
 
-  $async.Future<$0.VerifyOtpResponse> verifyOtp_Pre($grpc.ServiceCall call, $async.Future<$0.VerifyOtpRequest> request) async {
+  $async.Future<$1.VerifyOtpResponse> verifyOtp_Pre($grpc.ServiceCall call, $async.Future<$1.VerifyOtpRequest> request) async {
     return verifyOtp(call, await request);
   }
 
-  $async.Future<$0.SendOtpResponse> sendOtpByEmail($grpc.ServiceCall call, $0.SendOtpRequest request);
-  $async.Future<$0.SendOtpResponse> sendOtpBySms($grpc.ServiceCall call, $0.SendOtpRequest request);
-  $async.Future<$0.VerifyOtpResponse> verifyOtp($grpc.ServiceCall call, $0.VerifyOtpRequest request);
+  $async.Future<$1.SendOtpResponse> sendOtpByEmail($grpc.ServiceCall call, $1.SendOtpRequest request);
+  $async.Future<$1.SendOtpResponse> sendOtpBySms($grpc.ServiceCall call, $1.SendOtpRequest request);
+  $async.Future<$1.VerifyOtpResponse> verifyOtp($grpc.ServiceCall call, $1.VerifyOtpRequest request);
 }
