720267567d2a8a7ce09b51a84219f08f6b5b6f21
Vlad Sumin <vladsum00@gmail.com>
2024-09-12 13:01:03 +0300

    upd

--- kyc_app_server/bin/server.dart
+++ kyc_app_server/bin/server.dart
@@ -12,7 +12,6 @@ Future<void> main(List<String> args) async {
       KycEndpoint(),
       OtpEndpoint(),
       UserEndpoint(),
-      OtpEndpoint(),
     ],
     codecRegistry: CodecRegistry(codecs: const [GzipCodec(), IdentityCodec()]),
   );
